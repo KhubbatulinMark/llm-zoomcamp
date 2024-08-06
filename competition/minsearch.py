29a0f45e18c1aa2dec15be6f6511609477fa1fd8
mark-khubbatulin <markhubbatulin@gmail.com>
2024-08-07 00:29:01 +0300

    add baseline

--- competition/minsearch.py
+++ competition/minsearch.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 import numpy as np
 
